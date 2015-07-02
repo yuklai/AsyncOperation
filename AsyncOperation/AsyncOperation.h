a3201d6b4fd6bb2416b5e3c1d86dd9ce04df3cd4
Yuk Lai Suen <yuklai.suen@gmail.com>
2015-07-01 20:49:08 -0700

    Empty project

---
+++ AsyncOperation/AsyncOperation.h
@@ -0,0 +1,19 @@
+//
+//  AsyncOperation.h
+//  AsyncOperation
+//
+//  Created by Yuk Lai Suen on 7/1/15.
+//  Copyright (c) 2015 OnDotDot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncOperation.
+FOUNDATION_EXPORT double AsyncOperationVersionNumber;
+
+//! Project version string for AsyncOperation.
+FOUNDATION_EXPORT const unsigned char AsyncOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncOperation/PublicHeader.h>
+
+
---
+++ AsyncOperationTests/AsyncOperationTests.m
@@ -0,0 +1,40 @@
+//
+//  AsyncOperationTests.m
+//  AsyncOperationTests
+//
+//  Created by Yuk Lai Suen on 7/1/15.
+//  Copyright (c) 2015 OnDotDot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AsyncOperationTests : XCTestCase
+
+@end
+
+@implementation AsyncOperationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
